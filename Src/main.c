a5cd518637d8805ad4d9f32f9a570493e39e242e
tez <dsotmo@proton.me>
2024-04-08 22:03:39 +0200

    uasrt2 rxtx

--- Src/main.c
+++ Src/main.c
@@ -9,13 +9,24 @@
 char key;
 
 int main(void){
+	RCC->AHB2ENR |= GPIOA_EN;
+
+	// set PA5 as output
+	GPIOA->MODER |= ( 1U << 10 );
+	GPIOA->MODER &= ~( 1U << 11 );
+
 	uart2_rxtx_init();
-	int idx = 0;
 
 	printf( ">> bare-metal uart tx\r\n" );
 
 	while( 1 ){
 		key = uart2_read();
+		printf( ">> received %c\r\n", key );
+
+		if( key == '1' )
+			GPIOA->ODR |= LED_PIN;
+		else
+			GPIOA->ODR &= ~LED_PIN;
 	}
 	return 0;
 }
